2c207d397e9d3ae9be9d7f3fee3e20b9ede8e0d0
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-27 16:20:48 +0900

    add start

---
+++ StartProject/StartTableView/StartTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StartTableView
+//
+//  Created by Tatsuya Yamamoto on 2019/12/27.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // 1. 使用する画像をAssetsに入れる
+    // 2. PokemonTableViewController を作成する
+    // 3. PokemonTableViewController に配列を作成
+    // 4. PokemonTableViewController に処理を記述
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
